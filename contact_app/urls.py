b5be230ac219aae6c69823356eeb4390b3c80ef0
SUEDA BIÇAKLIOĞLU <96630987+SueBckoglu@users.noreply.github.com>
2024-05-24 12:49:01 +0300

    last changes

--- contact_app/urls.py
+++
@@ -1,8 +0,0 @@
-# contact/urls.py
-
-from django.urls import path
-from contact_app.views import contact_form  # Import the contact view function
-
-urlpatterns = [
-    path('contact_form/', contact_form, name='k'),
-]
--- static/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StaticAppConfig(AppConfig):
-    name = 'static'
-    path = 'C:\\Users\\ekinf\\Desktop\\portfolio\\static'
